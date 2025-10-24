781d89a1dfba244c21f24bd99bd728307258c6c6
Alex Morgan <lexyym@gmail.com>
2025-10-24 11:56:17 -0400

    edits to instructions

--- src/routes/Instructions.svelte
+++ src/routes/Instructions.svelte
@@ -1,13 +1,14 @@
 <div class="helptext" v-show="visible">
-  Welcome to the Bus Alarm, a handy tool for helping you leave in just time to
-  catch your
-  <a href="https://mbta.com/" target="”_blank”">Boston-area MBTA</a>
-  bus, trolley, or other mode of transport. You don't want to miss the bus... but
-  nor do you want to stand by the side of the road, in the weather, much longer than
-  you need to. Bus Alarm keeps track of when that bus is expected, so you can spend
-  up to the last reasonable moment in your warm, dry home or office before scampering
-  to the bus stop (or T stop, or ferry dock...) just in time!
-  <p />
+  <p>
+    Welcome to the Bus Alarm, a handy tool for helping you leave in just time to
+    catch your
+    <a href="https://mbta.com/" target="”_blank”">Boston-area MBTA</a>
+    bus, trolley, or other mode of transport. You don't want to miss the bus... but
+    nor do you want to stand by the side of the road, in the weather, much longer
+    than you need to. Bus Alarm keeps track of when that bus is expected, so you
+    can spend up to the last reasonable moment in your warm, dry home or office before
+    scampering to the bus stop (or T stop, or ferry dock...) just in time!
+  </p>
   <div class="disclaimer">
     <h2>Disclaimer on the Bus Alarm predictions</h2>
     Bus Alarm advises you on when to head out the door based on a feed of real-time
@@ -53,67 +54,85 @@
     whether to scramble out the door now, or wait.
   </div>
   <h2>Setting up your Bus Alarms</h2>
-  To set up a new bus alarm, open the &ldquo;Route, Direction, and Stop Picker&rdquo;
-  at the bottom of the page. First, specify where you are traveling from. You could
-  enter latitude and longitude; or, look up the coordinates of any address by typing
-  it into the Search Near Address field and hitting the tab key; or, you can drag
-  your location into view inside the map.
-  <p />
-  Next, search for a route: a bus number, or a train or trolley line, or a boat.
-  Start typing the bus number, T color, train destination, or boat identifier into
-  the Route field, and pick from the list of matches. Pick the direction in which
-  you want to travel, then pick your most convenient stop from the up to 3 stops
-  closest to the given location. Optionally, you can enter a nickname for these settings&mdash;
-  &ldquo;bus to work&rdquo; for example.
-  <p />
-  As soon as you click &ldquo;Add Stop to Watch List&rdquo;, a new Bus Alarm is added
-  to your page. Your browser will remember your Bus Alarms.
+  <p>
+    To set up a new bus alarm, open the &ldquo;Route, Direction, and Stop
+    Picker&rdquo; at the bottom of the page. First, specify where you are
+    traveling from. You could enter latitude and longitude; or, look up the
+    coordinates of any address by typing it into the Search Near Address field
+    and hitting the tab key; or, you can drag your location into view inside the
+    map.
+  </p>
+  <p>
+    Next, search for a route: a bus number, or a train or trolley line, or a
+    boat. Start typing the bus number, T color, train destination, or boat
+    identifier into the Route field, and pick from the list of matches. Pick the
+    direction in which you want to travel, then pick your most convenient stop
+    from the up to 3 stops closest to the given location. Optionally, you can
+    enter a nickname for these settings&mdash; &ldquo;bus to work&rdquo; for
+    example.
+  </p>
+  <p>
+    As soon as you click &ldquo;Add Stop to Watch List&rdquo;, a new Bus Alarm
+    is added to your page. Your browser will remember your Bus Alarms.
+  </p>
   <h2>Focusing on your currently relevant Bus Alarm</h2>
-  Suppose you have 2 alarms, &ldquo;bus to work&rdquo; and &ldquo;bus to tennis club&rdquo;.
-  You have to go to work now; so the details on when the bus to the tennis club is
-  coming are, sadly, not relevant to you right now. You can minimize those details
-  by clicking on the nickname&mdash; click on the the &ldquo;bus to tennis club&rdquo;
-  text at the top of the box, and that box will collapse. You can re-expand it by
-  clicking on the nickname again.
-  <p />
-  You can re-order your list of bus alarms by dragging a nickname and dropping it
-  onto another nickname; the dragged Bus Alarm will move to the position in the list
-  where you dropped it.
-  <p />
-  If a particular Bus Alarm is no longer of interest, open the settings for that
-  Bus Alarm by clicking on the gear icon, and click the &ldquo;remove&rdquo; button.
+  <p>
+    Suppose you have 2 alarms, &ldquo;bus to work&rdquo; and &ldquo;bus to
+    tennis club&rdquo;. You have to go to work now; so the details on when the
+    bus to the tennis club is coming are, sadly, not relevant to you right now.
+    You can minimize those details by clicking on the nickname. Click on the
+    &ldquo;bus to tennis club&rdquo; text at the top of the box, and that box
+    will collapse. You can re-expand it by clicking on the nickname again.
+  </p>
+  <p>You can re-order your list of bus alarms by dragging and dropping.</p>
+  <p>
+    If a particular Bus Alarm is no longer of interest, open the settings for
+    that Bus Alarm by clicking on the gear icon, and click the
+    &ldquo;Delete&rdquo; button.
+  </p>
   <h2>Alerting yourself on when it's time to go</h2>
-  Click on the Settings button (with the gear icon) to configure when and how you're
-  alerted that it's time to go. As your selected bus (or other transit) approaches
-  your stop, the Bus Alarm will go on Green Alert, then Yellow Alert, then Red Alert,
-  and then disappear once you've missed it.
+  <p>
+    Click on the Settings button (with the gear icon) to configure when and how
+    you're alerted that it's time to go. As your selected bus (or other transit)
+    approaches your stop, the Bus Alarm will go on Green Alert, then Yellow
+    Alert, then Red Alert, and then disappear once you've missed it.
+  </p>
   <h3>Green Alert, and ignoring early buses</h3>
-  Should you be dressed by now? Do you have time to shampoo, or to cook eggs? Find
-  out in a glance by specifying when your Bus Alarm goes to Green Alert, which will
-  indicate when you need to switch gears from going around your usual business to
-  expecting the bus to come pretty soon.
-  <p />
-  Of course, there's no need for notifications for a bus that comes way earlier than
-  when you need to go, so you can check the option to not show you early buses (&ldquo;early&rdquo;
-  being defined as a bus predicted to come before the specified time).
+  <p>
+    Should you be dressed by now? Do you have time to shampoo, or to cook eggs?
+    Find out in a glance by specifying when your Bus Alarm goes to Green Alert,
+    which will indicate when you need to switch gears from going around your
+    usual business to expecting the bus to come pretty soon.
+  </p>
+  <p>
+    Of course, there's no need for notifications for a bus that comes way
+    earlier than when you need to go, so you can check the option to not show
+    you early buses (&ldquo;early&rdquo; being defined as a bus predicted to
+    come before the specified time).
+  </p>
   <h3>Yellow Alert</h3>
-  Let's say you want to give yourself 4 minutes for shoes, outerwear, and final bag
-  check, and 5 minutes for a leasurely walk to the bus stop. Adjust the Yellow Alert
-  time to 9 minutes to get the alert when it's time to hustle. To get an<em
-    >audible</em
-  >
-  alert, check the &ldquo;Beep during Yellow Alert&rdquo; option. (The Bus Alarm
-  will only beep during the time window that you specify.) Beeping is off by default,
-  and if you re-load this page, any audible alerts will be turned off.
+  <p>
+    Let's suppose, for example, that you want to give yourself 4 minutes for
+    shoes, outerwear, and final bag check, and 5 minutes for a leasurely walk to
+    the bus stop. You would adjust the Yellow Alert time to 9 minutes to be
+    alerted when it's time to get going. To get an <em>audible</em>
+    alert, check the &ldquo;Beep during Yellow Alert&rdquo; option. (The Bus Alarm
+    will only beep during the time window that you specify.) Beeping is off by default,
+    and if you re-load this page, any audible alerts will be turned off.
+  </p>
   <h3>Red Alerts, and ignoring buses you've missed</h3>
-  Suppose you want to allow 5 minutes to walk to the bus stop... although maybe,<em
-    >just maybe,</em
-  >
-  if you ran, and the bus is just a bit tardy, you could make it to a bus arriving
-  in 2 minutes. Set the Red Alert to start when the bus is 5 minutes away. Since
-  you've <em>probably</em> missed any bus that's predicted to come within 2 minutes,
-  you may want to check the option to not show &ldquo;immediate&rdquo; buses, and
-  define &ldquo;immediate&rdquo; as those coming within 2 minutes.
+  <p>
+    Suppose you want to allow 5 minutes to walk to the bus stop (although maybe,
+    <em> just maybe, </em>
+    if you ran, and the bus is just a bit tardy, you could make it to a bus arriving
+    in 2 minutes.) Set the Red Alert for when the bus is 5 minutes away, and aim
+    to walk out the door when you see red.
+  </p>
+  <p>
+    Since you've <em>probably</em> missed any bus that's predicted to come within
+    2 minutes, you may want to check the option to not show &ldquo;immediate&rdquo;
+    buses, and define &ldquo;immediate&rdquo; as those coming within 2 minutes.
+  </p>
 </div>
 
 <style>
