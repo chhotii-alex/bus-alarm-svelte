370167bab9e8ea37936c9ad29718a7a39ba8ecd0
Alex Morgan <lexyym@gmail.com>
2025-10-26 15:11:23 -0400

    thinking about the audible alerts

---
+++ src/routes/Sound.svelte
@@ -0,0 +1,17 @@
+<script>
+  let audioElement;
+
+  function startSound() {
+    audioElement.play();
+  }
+
+  function stopSound() {
+    audioElement.pause();
+  }
+
+</script>
+
+
+<audio bind:this={audioElement} >
+  <source src="putthepathhere" type="audio/mpeg" />
+</audio>
